33d5207c7dffd7b67290875f5c3d59af8d735325
Chocoding1 <galmeagi2@naver.com>
2025-10-30 22:40:29 +0900

    Feat: 로또 발행 결과 출력 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,10 +4,12 @@ import java.util.List;
 import lotto.Lotto;
 import lotto.model.LottoService;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
 
     private InputView inputView = InputView.getInstance();
+    private OutputView outputView = OutputView.getInstance();
     private LottoService lottoService = LottoService.getInstance();
 
     public void playLotto() {
@@ -18,6 +20,7 @@ public class LottoController {
         List<Lotto> lottos = lottoService.publishLotto(purchasePrice);
 
         // 발행 결과 출력
+        outputView.printPublishedResult(lottos);
 
         // 당첨 번호 입력
         //보너스 번호 입력
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,27 @@
+package lotto.view;
+
+import java.util.List;
+import lotto.Lotto;
+
+public class OutputView {
+
+    private static OutputView instance;
+
+    private OutputView() {
+    }
+
+    public static OutputView getInstance() {
+        if (instance == null) {
+            instance = new OutputView();
+        }
+        return instance;
+    }
+
+    public void printPublishedResult(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+
+    }
+}
