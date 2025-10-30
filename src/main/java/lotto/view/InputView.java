5936dbd1d8f4a4187fbbc0f8ef2b3dcdcedba04b
Chocoding1 <galmeagi2@naver.com>
2025-10-30 17:08:21 +0900

    Refactor(InputView): 로또 구입 금액 입력 메서드 분리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,16 +5,32 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public int getPurchasePrice() {
-        System.out.println("구입금액을 입력해 주세요.");
-        String inputPrice = Console.readLine();
+        printGetPurchasePriceView();
+        String initialPurchasePrice = getInitialPurchasePrice();
 
-        int purchasePrice;
+        int purchasePrice = convertToInt(initialPurchasePrice);
+        validatePurchasePrice(purchasePrice);
+
+        return purchasePrice;
+    }
+
+    private static void printGetPurchasePriceView() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    private static String getInitialPurchasePrice() {
+        return Console.readLine();
+    }
+
+    private int convertToInt(String inputPrice) {
         try {
-            purchasePrice = Integer.parseInt(inputPrice);
+            return Integer.parseInt(inputPrice);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 구입금액은 숫자여야 합니다.");
         }
+    }
 
+    private void validatePurchasePrice(int purchasePrice) {
         if (purchasePrice < 1000) {
             throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 이상이어야 합니다.");
         }
@@ -22,7 +38,5 @@ public class InputView {
         if (purchasePrice % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 단위로 입력해주세요.");
         }
-
-        return purchasePrice;
     }
 }
