d2e1d373b48c0fb8c60352a16b205b9e9f758840
Chocoding1 <galmeagi2@naver.com>
2025-11-01 21:04:13 +0900

    Refactor(Lotto): 로또 번호 중복 검증 로직 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Lotto {
@@ -37,8 +38,20 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateLottoSize(numbers);
+        validateDuplicateNumbers(numbers);
+    }
+
+    private void validateLottoSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateDuplicateNumbers(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = Set.copyOf(numbers);
+        if (numbers.size() != uniqueNumbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복된 숫자를 포함하지 않아야 합니다.");
         }
     }
 
