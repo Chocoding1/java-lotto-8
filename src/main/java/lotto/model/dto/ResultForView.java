331637c39c0b19d7e44e446115a1ddeb592ed1c5
Chocoding1 <galmeagi2@naver.com>
2025-11-02 11:37:16 +0900

    Refactor: 출력용 결과 데이터 자료구조 HashMap으로 변경
    if-else 문의 반목을 없애기 위해
    일치하는 로또 번호 개수를 키로 가지는 HashMap 생성

--- src/main/java/lotto/model/dto/ResultForView.java
+++ src/main/java/lotto/model/dto/ResultForView.java
@@ -1,49 +1,37 @@
 package lotto.model.dto;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lotto.model.domain.CompareResult;
 
 public class ResultForView {
 
-    private int three = 0;
-    private int four = 0;
-    private int five = 0;
+    private final Map<Integer, Integer> resultCount = new HashMap<>();
     private int bonus = 0;
-    private int six = 0;
 
-    public int getThree() {
-        return three;
-    }
-
-    public int getFour() {
-        return four;
-    }
-
-    public int getFive() {
-        return five;
+    public Map<Integer, Integer> getResultCount() {
+        return resultCount;
     }
 
     public int getBonus() {
         return bonus;
     }
 
-    public int getSix() {
-        return six;
-    }
-
-    public void calculateStatistics(List<CompareResult> compareResults) {
+    public void getResultForView(List<CompareResult> compareResults) {
         for (CompareResult compareResult : compareResults) {
-            if (compareResult.getMatchCount() == 3) {
-                three++;
-            } else if (compareResult.getMatchCount() == 4) {
-                four++;
-            } else if (compareResult.getMatchCount() == 5) {
-                five++;
-            } else if (compareResult.getMatchCount() == 6 && compareResult.isBonusMatch()) {
-                bonus++;
-            } else if (compareResult.getMatchCount() == 6) {
-                six++;
+            int matchCount = compareResult.getMatchCount();
+            boolean bonusMatch = compareResult.isBonusMatch();
+
+            if (matchCount < 3) {
+                continue;
             }
+
+            if (matchCount == 6 && bonusMatch) {
+                bonus++;
+            }
+
+            resultCount.put(matchCount, resultCount.getOrDefault(matchCount, 0) + 1);
         }
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import java.util.Map;
 import lotto.model.domain.Lotto;
 import lotto.model.dto.ResultForView;
 import lotto.model.dto.PublishedLottoDto;
@@ -27,13 +28,14 @@ public class OutputView {
     }
 
     public void printWinningResult(ResultForView resultForView, double rateOfReturn) {
+        Map<Integer, Integer> result = resultForView.getResultCount();
         System.out.println("당첨 통계");
         System.out.println("---");
-        System.out.println("3개 일치 (5,000원) - " + resultForView.getThree() + "개");
-        System.out.println("4개 일치 (50,000원) - " + resultForView.getFour() + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + resultForView.getFive() + "개");
+        System.out.println("3개 일치 (5,000원) - " + result.getOrDefault(3, 0) + "개");
+        System.out.println("4개 일치 (50,000원) - " + result.getOrDefault(4, 0) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + result.getOrDefault(5, 0) + "개");
         System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + resultForView.getBonus() + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + resultForView.getSix() + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + result.getOrDefault(6, 0) + "개");
         System.out.println("총 수익률은 " + rateOfReturn + "%입니다.");
     }
 }
