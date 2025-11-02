375e36732d939295dc4a56cc63ef3f0548a49004
Chocoding1 <galmeagi2@naver.com>
2025-11-02 19:39:00 +0900

    Test: 테스트 파일 경로 변경

--- src/test/java/lotto/model/domain/PurchasePriceTest.java
+++ src/test/java/lotto/model/domain/PurchasePriceTest.java
@@ -1,6 +1,5 @@
-package lotto.model;
+package lotto.model.domain;
 
-import lotto.model.domain.PurchasePrice;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
--- src/test/java/lotto/view/InputViewTest.java
+++
@@ -1,9 +0,0 @@
-package lotto.view;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class InputViewTest {
-
-
-
-}
