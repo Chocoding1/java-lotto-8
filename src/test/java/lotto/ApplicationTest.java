ef427ee5983818fc3d0cb844b8f32cc23dff2bd5
Chocoding1 <galmeagi2@naver.com>
2025-11-03 02:31:23 +0900

    Test(ApplicationTest): 통합 테스트 작성

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -1,86 +1,86 @@
-//package lotto;
-//
-//import camp.nextstep.edu.missionutils.test.NsTest;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.List;
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.ValueSource;
-//
-//import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
-//import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//class ApplicationTest extends NsTest {
-//    private static final String ERROR_MESSAGE = "[ERROR]";
-//
-//    @Test
-//    void 기능_테스트() {
-//        assertRandomUniqueNumbersInRangeTest(
-//                () -> {
-//                    run("8000", "1,2,3,4,5,6", "7");
-//                    assertThat(output()).contains(
-//                            "8개를 구매했습니다.",
-//                            "[8, 21, 23, 41, 42, 43]",
-//                            "[3, 5, 11, 16, 32, 38]",
-//                            "[7, 11, 16, 35, 36, 44]",
-//                            "[1, 8, 11, 31, 41, 42]",
-//                            "[13, 14, 16, 38, 42, 45]",
-//                            "[7, 11, 30, 40, 42, 43]",
-//                            "[2, 13, 22, 32, 38, 45]",
-//                            "[1, 3, 5, 14, 22, 45]",
-//                            "3개 일치 (5,000원) - 1개",
-//                            "4개 일치 (50,000원) - 0개",
-//                            "5개 일치 (1,500,000원) - 0개",
-//                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
-//                            "6개 일치 (2,000,000,000원) - 0개",
-//                            "총 수익률은 62.5%입니다."
-//                    );
-//                },
-//                List.of(8, 21, 23, 41, 42, 43),
-//                List.of(3, 5, 11, 16, 32, 38),
-//                List.of(7, 11, 16, 35, 36, 44),
-//                List.of(1, 8, 11, 31, 41, 42),
-//                List.of(13, 14, 16, 38, 42, 45),
-//                List.of(7, 11, 30, 40, 42, 43),
-//                List.of(2, 13, 22, 32, 38, 45),
-//                List.of(1, 3, 5, 14, 22, 45)
-//        );
-//    }
-//
-//    @ParameterizedTest
-//    @DisplayName("로또 구입 금액 예외 테스트")
-//    @ValueSource(strings = {"1000j", "-2000", "0", "1550", " ", "\t"})
-//    void 예외_테스트_1(String price) {
-//        assertSimpleTest(() -> {
-//            runException(price);
-//            assertThat(output()).contains(ERROR_MESSAGE);
-//        });
-//    }
-//
-//    @ParameterizedTest
-//    @DisplayName("당첨 번호 예외 테스트")
-//    @ValueSource(strings = {"1,2,3,4,5*6", "1,2,3,4,5", "0,1,2,3,4,5", "1,2,3,4,5,5", " ", "\t"})
-//    void 예외_테스트_2(String numbers) {
-//        assertSimpleTest(() -> {
-//            runException("8000", numbers);
-//            assertThat(output()).contains(ERROR_MESSAGE);
-//        });
-//    }
-//
-//    @ParameterizedTest
-//    @DisplayName("보너스 번호 예외 테스트")
-//    @ValueSource(strings = {"a", "-1", "6", " ", "\t"})
-//    void 예외_테스트_3(String bonusNumber) {
-//        assertSimpleTest(() -> {
-//            runException("8000", "1,2,3,4,5,6", bonusNumber);
-//            assertThat(output()).contains(ERROR_MESSAGE);
-//        });
-//    }
-//
-//    @Override
-//    public void runMain() {
-//        Application.main(new String[]{});
-//    }
-//}
+package lotto;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ApplicationTest extends NsTest {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @Test
+    void 기능_테스트() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("8000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "8개를 구매했습니다.",
+                            "[8, 21, 23, 41, 42, 43]",
+                            "[3, 5, 11, 16, 32, 38]",
+                            "[7, 11, 16, 35, 36, 44]",
+                            "[1, 8, 11, 31, 41, 42]",
+                            "[13, 14, 16, 38, 42, 45]",
+                            "[7, 11, 30, 40, 42, 43]",
+                            "[2, 13, 22, 32, 38, 45]",
+                            "[1, 3, 5, 14, 22, 45]",
+                            "3개 일치 (5,000원) - 1개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 0개",
+                            "총 수익률은 62.5%입니다."
+                    );
+                },
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38),
+                List.of(7, 11, 16, 35, 36, 44),
+                List.of(1, 8, 11, 31, 41, 42),
+                List.of(13, 14, 16, 38, 42, 45),
+                List.of(7, 11, 30, 40, 42, 43),
+                List.of(2, 13, 22, 32, 38, 45),
+                List.of(1, 3, 5, 14, 22, 45)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("로또 구입 금액 예외 테스트")
+    @ValueSource(strings = {"1000j", "-2000", "0", "1550", " ", "\t"})
+    void 예외_테스트_1(String price) {
+        assertSimpleTest(() -> {
+            runException(price);
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @ParameterizedTest
+    @DisplayName("당첨 번호 예외 테스트")
+    @ValueSource(strings = {"1,2,3,4,5*6", "1,2,3,4,5", "0,1,2,3,4,5", "1,2,3,4,5,5", " ", "\t"})
+    void 예외_테스트_2(String numbers) {
+        assertSimpleTest(() -> {
+            runException("8000", numbers);
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @ParameterizedTest
+    @DisplayName("보너스 번호 예외 테스트")
+    @ValueSource(strings = {"a", "-1", "6", " ", "\t"})
+    void 예외_테스트_3(String bonusNumber) {
+        assertSimpleTest(() -> {
+            runException("8000", "1,2,3,4,5,6", bonusNumber);
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
