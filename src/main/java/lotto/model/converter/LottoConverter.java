83fcce4f3718d6f9f0d3c8c67ea6d8ad70180450
Chocoding1 <galmeagi2@naver.com>
2025-11-02 19:45:24 +0900

    Refactor(Lotto): 파일 경로 변경

--- src/main/java/lotto/model/converter/LottoConverter.java
+++ src/main/java/lotto/model/converter/LottoConverter.java
@@ -1,7 +1,7 @@
 package lotto.model.converter;
 
 import java.util.Arrays;
-import lotto.Lotto;
+import lotto.model.domain.Lotto;
 import lotto.model.validator.LottoNumberValidator;
 import lotto.util.InputUtil;
 
--- src/main/java/lotto/model/dto/PublishedLottoDto.java
+++ src/main/java/lotto/model/dto/PublishedLottoDto.java
@@ -1,7 +1,7 @@
 package lotto.model.dto;
 
 import java.util.List;
-import lotto.Lotto;
+import lotto.model.domain.Lotto;
 
 public class PublishedLottoDto {
 
