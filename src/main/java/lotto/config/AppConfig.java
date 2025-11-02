f0bea9bfebbfe2b8c75c92dc02c8ca3217ec5922
Chocoding1 <galmeagi2@naver.com>
2025-11-03 01:10:37 +0900

    Refactor(AppConfig): 객체 생성 및 의존 관리를 한 곳에서 처리하도록 변경

---
+++ src/main/java/lotto/config/AppConfig.java
@@ -0,0 +1,70 @@
+package lotto.config;
+
+import lotto.controller.LottoController;
+import lotto.model.converter.LottoConverter;
+import lotto.model.service.LottoComparator;
+import lotto.model.service.LottoPublisher;
+import lotto.model.service.PrizeCalculator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class AppConfig {
+
+    private LottoController lottoController;
+    private InputView inputView;
+    private OutputView outputView;
+    private LottoPublisher lottoPublisher;
+    private LottoComparator lottoComparator;
+    private LottoConverter lottoConverter;
+    private PrizeCalculator prizeCalculator;
+
+    public LottoController lottoController() {
+        if (lottoController == null) {
+            lottoController = new LottoController(inputView(), outputView(), lottoPublisher(), lottoComparator(),
+                    lottoConverter(), prizeCalculator());
+        }
+        return lottoController;
+    }
+
+    public InputView inputView() {
+        if (inputView == null) {
+            inputView = new InputView();
+        }
+        return inputView;
+    }
+
+    public OutputView outputView() {
+        if (outputView == null) {
+            outputView = new OutputView();
+        }
+        return outputView;
+    }
+
+    public LottoPublisher lottoPublisher() {
+        if (lottoPublisher == null) {
+            lottoPublisher = new LottoPublisher();
+        }
+        return lottoPublisher;
+    }
+
+    public LottoComparator lottoComparator() {
+        if (lottoComparator == null) {
+            lottoComparator = new LottoComparator();
+        }
+        return lottoComparator;
+    }
+
+    public LottoConverter lottoConverter() {
+        if (lottoConverter == null) {
+            lottoConverter = new LottoConverter();
+        }
+        return lottoConverter;
+    }
+
+    public PrizeCalculator prizeCalculator() {
+        if (prizeCalculator == null) {
+            prizeCalculator = new PrizeCalculator();
+        }
+        return prizeCalculator;
+    }
+}
