d557ac6ddfb83f0b5888c6b7b70bfcf04b0959b5
Chocoding1 <galmeagi2@naver.com>
2025-11-03 17:46:55 +0900

    Refactor(LottoConstant): 로또 관련 상수 추가

--- src/main/java/lotto/model/constant/LottoConstant.java
+++ src/main/java/lotto/model/constant/LottoConstant.java
@@ -5,4 +5,16 @@ public class LottoConstant {
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 45;
     public static final int LOTTO_LENGTH = 6;
+
+    public static final int THREE_MATCH = 3;
+    public static final int FOUR_MATCH = 4;
+    public static final int FIVE_MATCH = 5;
+    public static final int SIX_MATCH = 6;
+
+    public static final int NOT_WINNING_PRIZE = 0;
+    public static final int THREE_MATCH_PRIZE = 5000;
+    public static final int FOUR_MATCH_PRIZE = 50000;
+    public static final int FIVE_MATCH_PRIZE = 1500000;
+    public static final int BONUS_MATCH_PRIZE = 30000000;
+    public static final int SIX_MATCH_PRIZE = 2000000000;
 }
