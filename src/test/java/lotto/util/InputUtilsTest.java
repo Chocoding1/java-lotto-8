f332eabbaa2a87d7fb0a7f4d7816b676ab2f972c
Chocoding1 <galmeagi2@naver.com>
2025-11-03 13:06:03 +0900

    Chore(InputUtilsTest): 클래스명 변경

--- src/test/java/lotto/util/InputUtilsTest.java
+++ src/test/java/lotto/util/InputUtilsTest.java
@@ -1,19 +1,17 @@
 package lotto.util;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class InputUtilTest {
+class InputUtilsTest {
 
     @ParameterizedTest
     @DisplayName("정수 형태의 입력값이 아니면 예외 발생")
     @ValueSource(strings = {"ab", "1*2", ":3", "(", " ", "\t"})
     void throw_exception_when_invalid_input(String inputValue) {
-        Assertions.assertThatThrownBy(() -> InputUtil.convertToInt(inputValue))
+        Assertions.assertThatThrownBy(() -> InputUtils.convertToInt(inputValue))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }
