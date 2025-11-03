19cf4332768a46c24821da537c1805ee20d4a85d
Chocoding1 <galmeagi2@naver.com>
2025-11-03 20:00:43 +0900

    Refactor(LottoNumberUtil): 클래스 final 선언 및 생성자 private 설정

--- src/main/java/lotto/util/LottoNumberUtil.java
+++ src/main/java/lotto/util/LottoNumberUtil.java
@@ -4,7 +4,10 @@ import static lotto.model.constant.ErrorMessage.INVALID_LOTTO_NUMBER_RANGE;
 import static lotto.model.constant.LottoConstant.MAX_NUMBER;
 import static lotto.model.constant.LottoConstant.MIN_NUMBER;
 
-public class LottoNumberUtil {
+public final class LottoNumberUtil {
+
+    private LottoNumberUtil() {
+    }
 
     public static void validateNumber(int number) {
         if (number < MIN_NUMBER || MAX_NUMBER < number) {
