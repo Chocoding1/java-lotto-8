9998c6a50a3cfe23fba87d5e263c718201fdca9f
Chocoding1 <galmeagi2@naver.com>
2025-11-03 18:26:58 +0900

    Test(WinningLotto): 단위 테스트 작성

---
+++ src/test/java/lotto/model/domain/WinningLottoTest.java
@@ -0,0 +1,39 @@
+package lotto.model.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningLottoTest {
+
+
+    @Test
+    @DisplayName("보너스 번호 정상 초기화")
+    void addBonusNumber_success() {
+        // given
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+
+        // when
+        winningLotto.addBonusNumber(bonusNumber);
+
+        // then
+        Assertions.assertThat(winningLotto.getBonusNumber()).isEqualTo(bonusNumber);
+    }
+
+    @Test
+    @DisplayName("로또 번호와 중복된 보너스 번호 입력 시 예외 발생")
+    void addBonusNumber_fail_when_duplicate_bonus_number() {
+        // given
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6));
+        int duplicateBonusNumber = 6;
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> winningLotto.addBonusNumber(duplicateBonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+}
