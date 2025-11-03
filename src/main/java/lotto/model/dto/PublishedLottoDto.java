3078f2fd507e1f2b03507b0af81d82607e11a1c5
Chocoding1 <galmeagi2@naver.com>
2025-11-03 20:26:31 +0900

    Refactor(PublishedLottoDto): 필드 변경

--- src/main/java/lotto/model/dto/PublishedLottoDto.java
+++ src/main/java/lotto/model/dto/PublishedLottoDto.java
@@ -1,31 +1,23 @@
 package lotto.model.dto;
 
 import java.util.List;
-import lotto.model.domain.Lotto;
-import lotto.model.domain.PublishedLotto;
+import lotto.model.domain.lotto.PublishedLotto;
 
 public class PublishedLottoDto {
 
     private final int lottoCount;
-    private final List<Lotto> lottos;
     private final List<List<Integer>> lottoNumbers;
 
     public PublishedLottoDto(PublishedLotto publishedLotto) {
         this.lottoCount = publishedLotto.getLottoCount();
-        convertToInts(publishedLotto);
-        this.lottos = publishedLotto.getLottos();
+        this.lottoNumbers = publishedLotto.getLottoNumbers();
     }
 
     public int getLottoCount() {
         return lottoCount;
     }
 
-    public List<Lotto> getLottos() {
-        return lottos;
-    }
-
-    private void convertToInts(PublishedLotto publishedLotto) {
-        publishedLotto.getLottos().stream()
-                .map(Lotto::get)
+    public List<List<Integer>> getLottoNumbers() {
+        return lottoNumbers;
     }
 }
