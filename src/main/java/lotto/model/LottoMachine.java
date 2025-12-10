cbc479556339f1a2e407eaabfee3ed3442ca4967
Chocoding1 <galmeagi2@naver.com>
2025-12-10 17:44:50 +0900

    Refactor(LottoMachine): 로또 번호 추출 로직 메서드 분리

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.List;
 import lotto.Lotto;
 
 public class LottoMachine {
@@ -11,9 +12,13 @@ public class LottoMachine {
 
         ArrayList<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < issueCount; i++) {
-            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+            lottos.add(new Lotto(getLottoNumber()));
         }
 
         return new IssuedLotto(lottos);
+    }
+
+    private static List<Integer> getLottoNumber() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 }
