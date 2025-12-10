eda13a8a371dcc01a6e330ff03a95e52474e0f4e
Chocoding1 <galmeagi2@naver.com>
2025-12-10 17:45:16 +0900

    Refactor(PurchasePrice): 매직넘버 상수화

--- src/main/java/lotto/model/PurchasePrice.java
+++ src/main/java/lotto/model/PurchasePrice.java
@@ -29,7 +29,7 @@ public class PurchasePrice {
     }
 
     private void validateUnits(int price) {
-        if (price % 1000 != 0) {
+        if (price % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.");
         }
     }
